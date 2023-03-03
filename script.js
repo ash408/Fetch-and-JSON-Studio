1872adb1c1e3618ecc58d69c1bc42d893e18afe9
JaneDoe <janedoe@notanemail.com>
2023-03-02 20:50:21 -0500

    Bonus mission finished

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 window.addEventListener("load", function(){
    const container = document.getElementById("container");
+   const header = document.getElementById("header");
 
    async function fetchAstronauts(){
       const response = await fetch("http://handlers.education.launchcode.org/static/astronauts.json");
@@ -9,20 +10,28 @@ window.addEventListener("load", function(){
       return data;
    }
 
-   fetchAstronauts().then(function(data){
-      for (const astronaut of data) {
+   fetchAstronauts().then(function(astronauts){
+      const numAstronauts = astronauts.length
+      astronauts.sort(function(a, b){
+         return parseInt(a.hoursInSpace) - parseInt(b.hoursInSpace)
+      });
+
+      
+      for (const astronaut of astronauts) {
 	 let skillsString = ''
          for (skill of astronaut.skills){
             skillsString += skill + ', '
          }
 	 skillsString = skillsString.substring(0, skillsString.length - 2)
 
+	 let activity = astronaut.active ? 'active' : 'inactive';
+
 	 htmlString = `<div class="astronaut">
    <div class="bio">
       <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
       <ul>
          <li>Hours in space: ${astronaut.hoursInSpace}</li>
-         <li>Active: ${astronaut.active}</li>
+         <li class="${activity}">Active: ${astronaut.active}</li>
          <li>Skills: ${skillsString}</li>
       </ul>
    </div>
@@ -30,5 +39,7 @@ window.addEventListener("load", function(){
 </div>`
          container.insertAdjacentHTML('afterend', htmlString) 
       }
+     
+      header.textContent += `: ${numAstronauts}`
    });
 });
